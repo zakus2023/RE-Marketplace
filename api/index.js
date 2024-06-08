9ab213f5ab90ee457ee7d0ae3178708dc453ab46
abdul <idbsch2012@gmail.com>
2024-06-08 15:32:39 -0400

    created error handling middleware

--- api/index.js
+++ api/index.js
@@ -2,20 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import User from "./models/user.model.js";
- 
-
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGO
-  )
+  .connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to mongodb");
   })
@@ -27,17 +23,34 @@ app.listen(3000, () => {
   console.log("App litening on port 3000");
 });
 
-
 //Signup api
 //...........................................................................................
-app.post('/api/signup', async (req,res)=>{
-  const {username, email, password} = req.body
-  const hashedPassword = bcrypt.hashSync(password, 10)
-  const newUser = new User({username, email, password: hashedPassword})
+app.post("/api/signup", async (req, res,next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    await newUser.save()
-    res.status(200).json("User created successfully")
+    await newUser.save();
+    res.status(200).json("User created successfully");
   } catch (error) {
-    res.status(500).json(error.message)
+    //res.status(500).json(error.message);
+    next(error)
   }
-})
+});
+
+
+
+
+
+//................................................................................
+// Error handling middleware
+//................................................................................
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
